dbd538723bb82ce505ff1647694b9a18dd3fdf43
Hamza77html <125993127+Hamza77html@users.noreply.github.com>
2023-04-23 00:14:20 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("display");
+
+function insert(value) {
+
+  display.value += value;
+
+}
+
+function clearDisplay() {
+
+  display.value = "";
+
+}
+
+function backspace() {
+
+  display.value = display.value.slice(0, -1);
+
+}
+
+function calculate() {
+
+  let result = eval(display.value);
+
+  display.value = result;
+
+}
+
